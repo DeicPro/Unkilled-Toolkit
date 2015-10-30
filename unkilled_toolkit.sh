e5efad482f204703b73522238b3a96d20b447716
Deic <DeicPro@users.noreply.github.com>
2015-10-30 12:34:13 +0100

    busybox install removed & bugfixes
    -run busybox applets changed
    -some changes

--- unkilled_toolkit.sh
+++ unkilled_toolkit.sh
@@ -9,15 +9,15 @@ title(){
 }
 
 wait_input(){
-    $script_dir/stty cbreak -echo
-    i=$($script_dir/dd bs=1 count=1 2>/dev/null)
-    $script_dir/stty -cbreak echo
+    $script_dir/busybox stty cbreak -echo
+    i=$($script_dir/busybox dd bs=1 count=1 2>/dev/null)
+    $script_dir/busybox stty -cbreak echo
 }
 
 wait_input_two(){
-    $script_dir/stty cbreak -echo
-    j=$($script_dir/dd bs=1 count=1 2>/dev/null)
-    $script_dir/stty -cbreak echo
+    $script_dir/busybox stty cbreak -echo
+    j=$($script_dir/busybox dd bs=1 count=1 2>/dev/null)
+    $script_dir/busybox stty -cbreak echo
 }
 
 main_menu(){
@@ -152,19 +152,19 @@ Resolution:
         sleep 1
     fi
     echo Boosting performance...
-    chmod 660 /sys/module/lowmemorykiller/parameters/minfree
-    $script_dir/cat /sys/module/lowmemorykiller/parameters/minfree > /data/local/tmp/minfree
+    $script_dir/busybox chmod 660 /sys/module/lowmemorykiller/parameters/minfree
+    $script_dir/busybox cat /sys/module/lowmemorykiller/parameters/minfree > /data/local/tmp/minfree
     echo 10393,14105,18188,27468,31552,37120 > /sys/module/lowmemorykiller/parameters/minfree
-    chmod 220 /sys/module/lowmemorykiller/parameters/minfree
-    if mv /dev/random /dev/random.orig; then
-        $script_dir/ln -s /dev/urandom /dev/random
+    $script_dir/busybox chmod 220 /sys/module/lowmemorykiller/parameters/minfree
+    if $script_dir/busybox mv /dev/random /dev/random.orig; then
+        $script_dir/busybox ln -s /dev/urandom /dev/random
     fi
-    $script_dir/cat /proc/sys/kernel/random/read_wakeup_threshold > /data/local/tmp/read_wakeup_threshold
-    $script_dir/cat /proc/sys/kernel/random/write_wakeup_threshold > /data/local/tmp/write_wakeup_threshold
-    $script_dir/cat /proc/sys/kernel/randomize_va_space > /data/local/tmp/randomize_va_space
-    $script_dir/sysctl -qw kernel.random.read_wakeup_threshold=4096
-    $script_dir/sysctl -qw kernel.random.write_wakeup_threshold=4096
-    $script_dir/sysctl -qw kernel.randomize_va_space=0
+    $script_dir/busybox cat /proc/sys/kernel/random/read_wakeup_threshold > /data/local/tmp/read_wakeup_threshold
+    $script_dir/busybox cat /proc/sys/kernel/random/write_wakeup_threshold > /data/local/tmp/write_wakeup_threshold
+    $script_dir/busybox cat /proc/sys/kernel/randomize_va_space > /data/local/tmp/randomize_va_space
+    $script_dir/busybox sysctl -qw kernel.random.read_wakeup_threshold=4096
+    $script_dir/busybox sysctl -qw kernel.random.write_wakeup_threshold=4096
+    $script_dir/busybox sysctl -qw kernel.randomize_va_space=0
     am kill-all 2>/dev/null
     sleep 1
     echo Running Unkilled...
@@ -175,24 +175,24 @@ Resolution:
     unset j
     while clear; do
         if [ "$j" == c ] || [ "$j" == C ]; then
-            $script_dir/cat /data/local/tmp/minfree > /sys/module/lowmemorykiller/parameters/minfree
-            if rm -f /dev/random; then
-                mv /dev/random.orig /dev/random
+            $script_dir/busybox cat /data/local/tmp/minfree > /sys/module/lowmemorykiller/parameters/minfree
+            if $script_dir/busybox rm -f /dev/random; then
+                $script_dir/busybox mv /dev/random.orig /dev/random
             fi
-            $script_dir/sysctl -qw kernel.random.read_wakeup_threshold=$($script_dir/cat /data/local/tmp/read_wakeup_threshold)
-            $script_dir/sysctl -qw kernel.random.write_wakeup_threshold=$($script_dir/cat /data/local/tmp/write_wakeup_threshold)
-            $script_dir/sysctl -qw kernel.randomize_va_space=$($script_dir/cat /data/local/tmp/randomize_va_space)
+            $script_dir/busybox sysctl -qw kernel.random.read_wakeup_threshold=$($script_dir/busybox cat /data/local/tmp/read_wakeup_threshold)
+            $script_dir/busybox sysctl -qw kernel.random.write_wakeup_threshold=$($script_dir/busybox cat /data/local/tmp/write_wakeup_threshold)
+            $script_dir/busybox sysctl -qw kernel.randomize_va_space=$($script_dir/busybox cat /data/local/tmp/randomize_va_space)
             break
         else
             echo 'Write [C] and press [ENTER] to stop boost...'
-            $script_dir/sysctl -qw vm.drop_caches=3
+            $script_dir/busybox sysctl -qw vm.drop_caches=3
             read -t 60 j
         fi
     done
     if [ "$i" == 1 ] || [ "$i" == 2 ] || [ "$i" == 3 ] || [ "$i" == 4 ]; then
         wm size reset
     fi
-    $script_dir/kill -9 $(com.madfingergames.unkilled)
+    $script_dir/busybox kill -9 $($script_dir/busybox pgrep com.madfingergames.unkilled)
     sleep 1
     echo Done.
     sleep 1
@@ -259,8 +259,8 @@ Press any key to continue..."
 
         wait_input
     else
-        get_android_id=$($script_dir/cat $android_id_file)
-        $script_dir/cat > /data/local/tmp/unkilled_restore_android_id.sh <<-EOF
+        get_android_id=$($script_dir/busybox cat $android_id_file)
+        $script_dir/busybox cat > /data/local/tmp/unkilled_restore_android_id.sh <<-EOF
 #!/system/bin/sh
 #Script to restore Android ID
 
@@ -275,9 +275,9 @@ while clear; do
     echo "${red}-= Unkilled Toolkit =-${nc}
 
 Reboot to apply changes. Reboot now? [Y/N]"
-    $script_dir/stty cbreak -echo
+    $script_dir/busybox stty cbreak -echo
     i=replace1
-    $script_dir/stty -cbreak echo
+    $script_dir/busybox stty -cbreak echo
     case replace2 in
         y|Y)
             reboot
@@ -293,8 +293,8 @@ Reboot to apply changes. Reboot now? [Y/N]"
     esac
 done
 EOF
-        $script_dir/sed -i 's/replace1/$(\/data\/local\/unkilled_toolkit\/dd bs=1 count=1 2>\/dev\/null)/' /data/local/tmp/unkilled_restore_android_id.sh
-        $script_dir/sed -i 's/replace2/$i/' /data/local/tmp/unkilled_restore_android_id.sh
+        $script_dir/busybox sed -i 's/replace1/$(\/data\/local\/unkilled_toolkit\/busybox dd bs=1 count=1 2>\/dev\/null)/' /data/local/tmp/unkilled_restore_android_id.sh
+        $script_dir/busybox sed -i 's/replace2/$i/' /data/local/tmp/unkilled_restore_android_id.sh
         sh /data/local/tmp/unkilled_restore_android_id.sh
     fi
 }
@@ -304,7 +304,7 @@ sh_ota(){ # v2.1_custom By Deic
     version=v1.0.1
     cloud=https://github.com/DeicPro/Download/releases/download/cloud/unkilled_toolkit.version
     # Optional
-    notes_cloud=https://github.com/DeicPro/Download/releases/download/cloud/unkilled_toolkit.changelog
+    changelog_cloud=https://github.com/DeicPro/Download/releases/download/cloud/unkilled_toolkit.changelog
     # 0/1 = Disabled/Enabled
     show_version=1
     show_changelog=1
@@ -350,7 +350,7 @@ sh_ota(){ # v2.1_custom By Deic
         if [ "$arch" == x86 ]; then
             file_location=$EXTERNAL_STORAGE/download
         else
-            $script_dir/curl -k -L -s -o /data/local/tmp/unkilled_toolkit.changelog $notes_cloud
+            $script_dir/curl -k -L -s -o /data/local/tmp/unkilled_toolkit.changelog $changelog_cloud
             file_location=/data/local/tmp
         fi
     fi
@@ -360,21 +360,21 @@ sh_ota(){ # v2.1_custom By Deic
 
             title
 
-            if [ "$($script_dir/grep $version $file_location/unkilled_toolkit.version 2>/dev/null)" ]; then
+            if [ "$($script_dir/busybox grep $version $file_location/unkilled_toolkit.version 2>/dev/null)" ]; then
                 echo You have the latest version.
                 sleep 1
                 install=0
                 break
             else
                 if [ "$show_version" == 1 ]; then
-                    version_opt=": $($script_dir/cat $file_location/unkilled_toolkit.version 2>/dev/null | $script_dir/tr '\n' ',' | $script_dir/cut -d ',' -f1)"
+                    version_opt=": $($script_dir/busybox cat $file_location/unkilled_toolkit.version 2>/dev/null | $script_dir/busybox tr '\n' ',' | $script_dir/busybox cut -d ',' -f1)"
                 else
                     version_opt=...
                 fi
                 echo "A new version of the script was found$version_opt
 "
                 if [ "$show_changelog" == 1 ] && [ -f $file_location/unkilled_toolkit.changelog ]; then
-                    $script_dir/cat $file_location/unkilled_toolkit.changelog
+                    $script_dir/busybox cat $file_location/unkilled_toolkit.changelog
                     echo
                 fi
                 echo 'Want install it? (Y/N)'
@@ -404,16 +404,16 @@ sh_ota(){ # v2.1_custom By Deic
 
         echo Downloading...
         if [ "$arch" == x86 ]; then
-            am start -a android.intent.action.VIEW $(cat $file_location/unkilled_toolkit.version | $script_dir/tr '\n' ',' | $script_dir/cut -d ',' -f2) >/dev/null 2>&1
+            am start -a android.intent.action.VIEW $($script_dir/busybox cat $file_location/unkilled_toolkit.version | $script_dir/busybox tr '\n' ',' | $script_dir/busybox cut -d ',' -f2) >/dev/null 2>&1
         else
-            $script_dir/curl -k -L -s -o $file_location/unkilled_toolkit.sh $($script_dir/cat $file_location/unkilled_toolkit.version | $script_dir/tr '\n' ',' | $script_dir/cut -d ',' -f2)
+            $script_dir/curl -k -L -s -o $file_location/unkilled_toolkit.sh $($script_dir/busybox cat $file_location/unkilled_toolkit.version | $script_dir/busybox tr '\n' ',' | $script_dir/busybox cut -d ',' -f2)
         fi
         sleep 1
     fi
     while :; do
         if [ "$install" == 0 ]; then
-            rm -f $file_location/unkilled_toolkit.version
-            rm -f $file_location/unkilled_toolkit.changelog
+            $script_dir/busybox rm -f $file_location/unkilled_toolkit.version
+            $script_dir/busybox rm -f $file_location/unkilled_toolkit.changelog
             break
         fi
         if [ -f $file_location/unkilled_toolkit.sh ]; then
@@ -422,14 +422,14 @@ sh_ota(){ # v2.1_custom By Deic
             sleep 5
             echo Installing...
             sleep 5
-            cp -f $file_location/unkilled_toolkit.sh $0
+            $script_dir/busybox cp -f $file_location/unkilled_toolkit.sh $0
             sleep 1
-            chmod 755 $0
-            rm -f $file_location/unkilled_toolkit.sh
+            $script_dir/busybox chmod 755 $0
+            $script_dir/busybox rm -f $file_location/unkilled_toolkit.sh
             echo Installed.
             sleep 1
-            rm -f $file_location/unkilled_toolkit.version
-            rm -f $file_location/unkilled_toolkit.changelog
+            $script_dir/busybox rm -f $file_location/unkilled_toolkit.version
+            $script_dir/busybox rm -f $file_location/unkilled_toolkit.changelog
             sh $0
             clear
             exit
@@ -493,9 +493,6 @@ else
             echo Setting up permissions...
             chmod -R 755 /data/local/unkilled_toolkit
             sleep 1
-            echo Installing...
-            $script_dir/busybox --install -s $script_dir
-            sleep 1
             echo Clean up downloaded file...
             rm -f $EXTERNAL_STORAGE/download/busybox.$arch
             sleep 1
@@ -505,15 +502,15 @@ else
         fi
     done
 fi
-if [ "$arch" == x86 ] || [ -f $EXTERNAL_STORAGE/download/unkilled_toolkit.sh ]; then
+if [ "$arch" == x86 ] && [ -f $EXTERNAL_STORAGE/download/unkilled_toolkit.sh ]; then
     echo Installing Unkilled Toolkit...
-    cp -f $EXTERNAL_STORAGE/download/unkilled_toolkit.sh /system/xbin/utk
+    $script_dir/busybox cp -f $EXTERNAL_STORAGE/download/unkilled_toolkit.sh /system/xbin/utk
     sleep 1
     echo Setting up permissions...
-    chmod 755 /system/xbin/utk
+    $script_dir/busybox chmod 755 /system/xbin/utk
     sleep 1
     echo Clean up downloaded file...
-    rm -f $EXTERNAL_STORAGE/download/unkilled_toolkit.sh
+    $script_dir/busybox rm -f $EXTERNAL_STORAGE/download/unkilled_toolkit.sh
     sleep 1
     echo Done.
     sleep 1
@@ -534,19 +531,19 @@ else
     sleep 1
     am start -a android.intent.action.VIEW $curl_cloud >/dev/null 2>&1
     while true; do
-        if [ "$($script_dir/wc -c $EXTERNAL_STORAGE/download/curl.arm 2>/dev/null | $script_dir/awk '{print $1}')" == 345616 ]; then
-            $script_dir/kill -9 $(pgrep com.android.browser) 2>/dev/null
-            $script_dir/kill -9 $(pgrep com.android.chrome) 2>/dev/null
+        if [ "$($script_dir/busybox wc -c $EXTERNAL_STORAGE/download/curl.arm 2>/dev/null | $script_dir/busybox awk '{print $1}')" == 345616 ]; then
+            $script_dir/busybox kill -9 $(pgrep com.android.browser) 2>/dev/null
+            $script_dir/busybox kill -9 $(pgrep com.android.chrome) 2>/dev/null
             sleep 1
             echo Copying cURL...
             sleep 1
-            cp -f $EXTERNAL_STORAGE/download/curl.arm $script_dir/curl
+            $script_dir/busybox cp -f $EXTERNAL_STORAGE/download/curl.arm $script_dir/curl
             sleep 1
             echo Setting up permissions...
-            chmod 755 $script_dir/curl
+            $script_dir/busybox chmod 755 $script_dir/curl
             sleep 1
             echo Cleaning up downloaded file...
-            rm -f $EXTERNAL_STORAGE/download/curl.arm
+            $script_dir/busybox rm -f $EXTERNAL_STORAGE/download/curl.arm
             sleep 1
             echo Done.
             sleep 1
@@ -561,14 +558,14 @@ else
     echo SQLite3 binary not found. Downloading...
     $script_dir/curl -k -L -s -o /data/local/tmp/sqlite3 $sqlite_cloud 2>/dev/null
     while true; do
-        if [ "$($script_dir/wc -c /data/local/tmp/sqlite3 2>/dev/null | $script_dir/awk '{print $1}')" == 877144 ]; then
+        if [ "$($script_dir/busybox wc -c /data/local/tmp/sqlite3 2>/dev/null | $script_dir/busybox awk '{print $1}')" == 877144 ]; then
             sleep 1
             echo Copying SQLite3...
             sleep 1
-            cp -f /data/local/tmp/sqlite3 /data/local/unkilled_toolkit
+            $script_dir/busybox cp -f /data/local/tmp/sqlite3 /data/local/unkilled_toolkit
             sleep 1
             echo Setting up permissions...
-            chmod 755 $script_dir/sqlite3
+            $script_dir/busybox chmod 755 $script_dir/sqlite3
             sleep 1
             echo Done.
             sleep 1
@@ -578,13 +575,13 @@ else
 fi
 if [ -f $EXTERNAL_STORAGE/download/unkilled_toolkit.sh ]; then
     echo Installing Unkilled Toolkit...
-    cp -f $EXTERNAL_STORAGE/download/unkilled_toolkit.sh /system/xbin/utk
+    $script_dir/busybox cp -f $EXTERNAL_STORAGE/download/unkilled_toolkit.sh /system/xbin/utk
     sleep 1
     echo Setting up permissions...
-    chmod 755 /system/xbin/utk
+    $script_dir/busybox chmod 755 /system/xbin/utk
     sleep 1
     echo Clean up downloaded file...
-    rm -f $EXTERNAL_STORAGE/download/unkilled_toolkit.sh
+    $script_dir/busybox rm -f $EXTERNAL_STORAGE/download/unkilled_toolkit.sh
     sleep 1
     echo Done.
     sleep 1
